5e154f4051d271cb17b8a697424bc41874b4a394
BaobhanSith <reece.tait04@gmail.com>
2023-06-10 14:06:22 +0100

    Added Window events

--- DarkEngine/src/Dark/Application.cpp
+++ DarkEngine/src/Dark/Application.cpp
@@ -1,14 +1,17 @@
 #include "dkpch.h"
 #include "Application.h"
 
-#include "Dark/Events/ApplicationEvent.h"
 #include "Dark/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Dark {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 	Application::~Application() {}
 
@@ -18,5 +21,17 @@ namespace Dark {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		DK_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
 	}
 }
--- DarkEngine/src/Dark/Application.h
+++ DarkEngine/src/Dark/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Dark/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Dark {
@@ -11,7 +13,10 @@ namespace Dark {
 		Application();
 		virtual ~Application();
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
